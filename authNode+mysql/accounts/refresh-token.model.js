4ec6314f9f8ed69ad388d0dcd795deda722346e6
cavalcant3 <jmatheusoc269@gmail.com>
2021-08-02 21:14:27 -0300

    configurações do token

---
+++ authNode+mysql/accounts/refresh-token.model.js
@@ -0,0 +1,37 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = model;
+
+function model(sequelize) {
+  const attributes = {
+    token: { type: DataTypes, STRING },
+    expires: { type: DataTypes.DATE },
+    created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    createdByIp: { type: DataTypes.STRING },
+    revoked: { type: DataTypes.DATE },
+    revokedByIp: { type: DataTypes.STRING },
+    replacedByToken: { type: DataTypes.STRING },
+    isExpired: {
+      type: DataTypes.VIRTUAL,
+      get() {
+        return Data.NOW() >= this.expires;
+      },
+    },
+    isActive: {
+      type: DataTypes.VIRTUAL,
+      get() {
+        return !this.revoked && !this.Expired;
+      },
+    },
+  };
+
+  const options = {
+    timestamps: false,
+  };
+}
+
+return sequelize.define("refreshToken", attributes, options);
